1a3072aa03c30e398b6bc7bfa64893c1e50c4cdf
blueSparrow2000 <amadeus@kaist.ac.kr>
2023-12-22 16:57:07 +0900

    commit before a lot of modification

--- chart_patterns.py
+++ chart_patterns.py
@@ -20,13 +20,14 @@ def special_hold(pos,line,point,length,special):  # here, length is in milliseco
     return "%s,%d,%d,%d,%d,%s\n"%('hold',pos,line,point,length,special)
 
 
-def strike_double(pos,line1,line2,point):
-    return
 
-def strike_triple(pos,line1,line2,point):
-    return
+# 이제부턴 basic strike나 special strike 녀석들이 이 함수를 부른다! (이전 차트 빌더들이랑 호환되게 함. 앞으로는 이 함수만 쓸듯)
+def write_multi_tiles(pattern, beat_pos,info):
+    multitile_info = "%s,%d"%(pattern,beat_pos)
 
+    for i in range(4): # for each lane (1~4)
+        info = ''
+        multitile_info += info
+        pass
 
-def hold_double(pos,line1,line2,point,length):
-    return
-
+    return multitile_info
