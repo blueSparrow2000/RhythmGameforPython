18faaf3811c86b0798f5131aae6d0df0feca3415
blueSparrow2000 <amadeus@kaist.ac.kr>
2023-12-24 20:58:52 +0900

    AI chart builder comming soon!

--- ai_chart_builder.py
+++ ai_chart_builder.py
@@ -1,2 +1,16 @@
+'''
+This file makes ai charts given a music!
+
+1. Music file given to music21 module
+2. Get script
+3. Translate script to chart pattern
+
+I made a code which does part 3.
+'''
+
+from music21 import *
+
+
+
 
 
